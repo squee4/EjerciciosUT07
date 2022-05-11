5bf46a80c4696d74d36ba9ea8d777a5aacb6354c
squee4 <five_senses@hotmail.fr>
2022-05-11 10:59:26 +0200

    commit

--- src/main/java/ejc04/Ejercicio04.java
+++ src/main/java/ejc04/Ejercicio04.java
@@ -1,20 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejc04;
 
-/**
- *
- * @author ismajj
- */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Ejercicio04 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Vehiculo> lista = new ArrayList();
+        
+        for (int i = 0; i < 30; i++) {
+            if(i <10){
+                lista.add(new Turismo());
+            } else if (i<20){
+                lista.add(new Deportivo());
+            } else {
+                lista.add(new Furgoneta());
+            }
+        }
+        
+        // Fichero a crear. Ruta relativa a la carpeta raíz del proyecto
+        String idFichero = "vehiculos.txt";
+
+        // Si se utiliza el constructor FileWriter(idFichero, true) entonces se anexa información
+        // al final del fichero idFichero
+        // Estructura try-with-resources. Instancia el objeto con el fichero a escribir
+        // y se encarga de cerrar el recurso "flujo" una vez finalizadas las operaciones
+        try (BufferedWriter flujo = new BufferedWriter(new FileWriter(idFichero))) {
+            for (Vehiculo vehiculo : lista) {
+                flujo.write(vehiculo.toString());
+                flujo.newLine();
+            }
+            // Metodo fluh() guarda cambios en disco 
+            flujo.flush();
+            System.out.println("Fichero " + idFichero + " creado correctamente.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
     
 }
--- src/main/java/ejc07/Ejercicio07.java
+++ src/main/java/ejc07/Ejercicio07.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejc07;
 
-/**
- *
- * @author ismajj
- */
 public class Ejercicio07 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
